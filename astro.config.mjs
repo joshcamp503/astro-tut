66e3f3951ea4992e8a817b46b7ea6f63394102c8
joshcamp503 <joshcamp503@gmail.com>
2025-08-19 14:44:39 -0700

    builds astro island and also adds a theme switcher using a script tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://joshs-astro-tut-netlify.com"
+  site: "https://joshs-astro-tut-netlify.com",
+  integrations: [preact()]
 });
